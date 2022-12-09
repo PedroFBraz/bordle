0d095ee2ab03886b867d9387100a93ced9fe8f75
Pedro F. Braz <brazpedrof@outlook.com>
2022-12-09 08:35:17 +0100

    Minor fix

--- bordle-server/util.h
+++ bordle-server/util.h
@@ -2,10 +2,10 @@
 #include <string>
 #include <cstdlib>
 
-	template <typename Error_handler>
-	void assert_(bool assertion, std::string error_message, Error_handler error_handler) {
-		if (!assert) {
-			error_handler(error_message);
-			std::exit(1);
-		}
+template <typename Error_handler>
+void assert_(bool assertion, std::string error_message, Error_handler error_handler) {
+	if (!assertion) {
+		error_handler(error_message);
+		std::exit(1);
 	}
+}
