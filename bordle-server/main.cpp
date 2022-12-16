01bcadbc4d9580696c433582cc946169a2a6ade0
Pedro F. Braz <brazpedrof@outlook.com>
2022-12-16 17:10:44 +0100

    bordle-server main.cpp:   The program now reads words from a word.txt file and inserts them into the database   Minor changes

--- bordle-server/main.cpp
+++ bordle-server/main.cpp
@@ -1,7 +1,10 @@
+#define BORDLE_DEBUG
 #include <string>
 #include <iostream>
 #include <format>
 #include <algorithm>
+#include <chrono>
+#include <fstream>
 #include "util.h"
 #include "toml.hpp"
 #include "sqlite3.h"
@@ -14,11 +17,15 @@
 	#error Unsupported operating system
 #endif
 
+
+
 void print_error(std::string error_message) {
 	std::cout << error_message << std::endl;
 }
 
+
 int main() {
+	
 	const auto data = toml::parse("config.toml");
 #pragma region Asserts
 	assert_(toml::find<long long>(data, "port") <= 65'535,
@@ -48,7 +55,7 @@ int main() {
 #pragma endregion
 	const auto port			= toml::find<unsigned short>(data, "port");
 	const auto max_attempts = toml::find<unsigned short>(data, "max_attempts");
-	const auto ip_address	= toml::find<std::string>(data, "ip_address");
+	//const auto ip_address	= toml::find<std::string>(data, "ip_address"); // What's this here for lol?
 	const auto db_name		= toml::find<std::string>(data, "db_name");
 	const auto create_db    = toml::find<bool>(data, "create_db");
 	const auto word_size	= toml::find<unsigned short>(data, "word_size");
@@ -58,17 +65,34 @@ int main() {
 		   "\nHint: You might never have created a database file."
 		   "\nHint: You might have set the create_db variable in config.toml to false.",
 		  print_error);
-
 	// Todo: Ensure that name isn't bigger than 255
 	std::string sql_statement = std::format("CREATE TABLE IF NOT EXISTS users (ip CHARACTER(32),"
 											"name CHARACTER(255), correct INT, wrong INT,"
 											"got_today_correct INT);"
 											"CREATE TABLE IF NOT EXISTS bordles (word VARCHAR({}),"
-											"date DATE);", word_size);
+											"date DATE,"
+											"UNIQUE(word));", word_size);
 	char*  sqlite_error_message = static_cast<char*>(sqlite3_malloc(1024));
 	std::fill(sqlite_error_message, sqlite_error_message + 1023, '\0');
-	sqlite3_exec(db, sql_statement.c_str(), nullptr, nullptr, &sqlite_error_message); 
-	assert_(!sqlite_error_message, sqlite_error_message, print_error);
+	sqlite3_exec(db, sql_statement.c_str(), nullptr, nullptr, &sqlite_error_message);
+	//assert_(!sqlite_error_message, sqlite_error_message, print_error); // Buggy
+	const auto current_time = std::chrono::system_clock::now().time_since_epoch().count();
+	constexpr auto day_in_seconds = 86'400;
+	std::ifstream words("words.txt");
+	assert_(words.is_open(), "Assertion failed! Failed to open words.txt", print_error); 
+	for (unsigned long long i = current_time; !words.eof(); i += day_in_seconds) {
+		std::string word;
+		words >> word;
+		//assert_(word.size() == word_size, std::vformat("Assertion failed! {:x} is {} than {}!", std::make_format_args(word, (word.size() > word_size ? "bigger" : "smaller"), word_size)), print_error);
+		std::string sql_statement = std::format("INSERT OR IGNORE INTO bordles VALUES ('{}', {});", word, i); // This probably isn't efficient
+		
+		//	If there is a last empty newline in words.txt, it gets inserted into the database.
+		if (!word.empty()) {
+			sqlite3_exec(db, sql_statement.c_str(), nullptr, nullptr, &sqlite_error_message);
+		}
+		//assert_(!sqlite_error_message, sqlite_error_message, print_error); // Buggy
+	}
+	words.close();
 	sqlite3_free(sqlite_error_message);
 	sqlite_error_message = nullptr;
 }
