a23b17afc9cc2d7c1319112b740261e9990f1edf
Pedro F. Braz <brazpedrof@outlook.com>
2022-12-17 09:03:58 +0100

    bordle-server main.cpp   Added the word_file variable to allow the user to use other file names rather than words.txt for their list of words.   Minor adjustments

--- bordle-server/main.cpp
+++ bordle-server/main.cpp
@@ -38,6 +38,10 @@ int main() {
 			"Assertion failed! word_size variable must not be greater than 65'535!");
 	assert_(toml::find<long long>(data, "word_size") > 0,
 			"Assertion failed! word_size variable must be greater than 0!");
+	assert_(toml::find<std::string>(data, "word_file").length() <= 1024,
+		"Assertion failed! word_file variable length must not be greater than 1024!");
+	assert_(!toml::find<std::string>(data, "word_file").empty(),
+		"Assertion failed! word_file variable must not be an empty string!");
 #pragma endregion
 	const auto port			= toml::find<unsigned short>(data, "port");
 	const auto max_attempts = toml::find<unsigned short>(data, "max_attempts");
@@ -45,6 +49,7 @@ int main() {
 	const auto db_name		= toml::find<std::string>(data, "db_name");
 	const auto create_db    = toml::find<bool>(data, "create_db");
 	const auto word_size	= toml::find<unsigned short>(data, "word_size");
+	const auto word_file    = toml::find<std::string>(data, "word_file");
 	sqlite3* db = nullptr;
 	sqlite3_open_v2(db_name.c_str(), &db, (create_db ? SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE : SQLITE_OPEN_READWRITE), BORDLE_VFS);
 	assert_(sqlite3_errcode(db) != SQLITE_CANTOPEN, "Assertion failed! Cannot open database file."
@@ -64,15 +69,15 @@ int main() {
 	//assert_(!sqlite_error_message, sqlite_error_message, print_error); // Buggy
 	const auto current_time = std::chrono::system_clock::now().time_since_epoch().count();
 	constexpr auto day_in_seconds = 86'400;
-	std::ifstream words("words.txt");
-	assert_(words.is_open(), "Assertion failed! Failed to open words.txt"); 
+	std::ifstream words(word_file);
+	assert_(words.is_open(), std::format("Assertion failed! Failed to open {}.", word_file)); 
 	for (unsigned long long i = current_time; !words.eof(); i += day_in_seconds) {
 		std::string word;
 		words >> word;
 		std::string sql_statement = std::format("INSERT OR IGNORE INTO bordles VALUES ('{}', {});", word, i); // This probably isn't efficient
 		spdlog::debug("Line {}: {}.", __LINE__, sqlite3_errmsg(db));
 		
-		//	If there is a last empty newline in words.txt, it gets inserted into the database without this.
+		//	If there is a last empty newline in the word file, it gets inserted into the database without this.
 		if (!word.empty()) {
 			if (word.size() != word_size) {
 				spdlog::warn("Length of \"{}\" ({}) is {} than {}. Skipping insertion of the word into the database.", word, word.size(), (word.size() > word_size ? "bigger" : "smaller"), word_size);
